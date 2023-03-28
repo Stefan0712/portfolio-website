69cc2b12a3bc290e9a2da1b8750ebb4328d1c55e
Stefan0712 <s.vladulescu@gmail.com>
2023-03-28 16:02:56 +0300

    Changed some colors and improved the readability

--- public/js/index.js
+++ public/js/index.js
@@ -5,18 +5,21 @@ const rs = getComputedStyle(r);
 const logo = document.querySelector('.logo');
 const hero = document.querySelector('.welcome-section');
 const btn = document.getElementById('btn-dot')
-
+const navContact = document.getElementById('nav-contact-btn')
+const homeContact = document.getElementById('home-contact')
 
 const toggleDarkMode = () =>{
     if(mode==='dark'){
         btn.classList.toggle('activeDarkModeBtn')
-        r.style.setProperty('--primary','white')
-        r.style.setProperty('--secondary','#ECF9FF')
+        r.style.setProperty('--primary','#FBFBFB')
+        r.style.setProperty('--secondary','#FEFBF3')
         r.style.setProperty('--el','black')
-        r.style.setProperty('--accent','#66BFBF')
+        r.style.setProperty('--accent','black')
         r.style.setProperty('--homeBtnText','white')
         logo.classList.toggle('logoLight')
         hero.classList.toggle('heroLight')
+        navContact.classList.toggle('lightHomeBtn')
+        homeContact.classList.toggle('lightHomeBtn')
         for(let i=0;i<icons.length;i++){
             icons[i].classList.toggle('darkIcon')
         }
@@ -25,12 +28,14 @@ const toggleDarkMode = () =>{
     }else if(mode==='light'){
         btn.classList.toggle('activeDarkModeBtn')
         r.style.setProperty('--primary','#1a1d22')
-        r.style.setProperty('--secondary','#24272c')
+        r.style.setProperty('--secondary','#FEFBF3')
         r.style.setProperty('--el','white')
         r.style.setProperty('--accent','#b82a34')
         r.style.setProperty('--homeBtnText','lightgray')
         logo.classList.toggle('logoLight')
         hero.classList.toggle('heroLight')
+        navContact.classList.toggle('lightHomeBtn')
+        homeContact.classList.toggle('lightHomeBtn')
         for(let i=0;i<icons.length;i++){
             icons[i].classList.toggle('darkIcon')
         }
